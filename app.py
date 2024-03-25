a554f9ddc66ce48ad59b45f067c4a3e10faa0d43
Chandresh Patel <100907336+Patel-Chandresh@users.noreply.github.com>
2024-03-25 16:05:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+#!pip install streamlit
+import os
+import pickle
+import streamlit as st
+#from streamlit_option_menu import option_menu
+
+# Set page configuration
+st.set_page_config(page_title="Health Assistant",
+                   layout="wide",
+                   page_icon="🧑‍⚕️")
